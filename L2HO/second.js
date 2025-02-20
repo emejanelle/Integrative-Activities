909950feb98a9c30df561a9930f0fbee372a330c
Mary Janelle C. Caparros <151702486+emejanelle@users.noreply.github.com>
2025-02-20 20:41:15 +0800

    Add files via upload

---
+++ L2HO/second.js
@@ -0,0 +1,27 @@
+let a;
+let b;
+let z;
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log (addNumbers(5,10));
+
+a = (prompt("First digit: "));
+b = (prompt("Second digit: "));
+
+if (confirm("Do you want to continue getting the sum of the digits? ") == true) {
+
+    z = addNumbers(Number(a),Number(b));
+
+    console.log("First digit: " + a);
+    console.log("Second digit: " + b);
+    console.log("The sum of " + a + " + " + b + " is " + z + ".");
+    alert("The sum of " + a + " and " + b + " is " + z + ".");
+} 
+else {
+    console.log("You cancelled adding the digits.")
+}
+
+
